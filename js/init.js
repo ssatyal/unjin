690c167a769bc076bb57e4e3bb2f09ebc3465a7b
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-05-20 22:37:13 -0400

    adds scrolling, changes navbar link desc to publications link

--- js/init.js
+++ js/init.js
@@ -5,3 +5,24 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+$(document).ready(function() {
+  $('a[href*=#]').each(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname
+    && this.hash.replace(/#/,'') ) {
+      var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) +']');
+      var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+       if ($target) {
+         var targetOffset = $target.offset().top;
+         $(this).click(function() {
+           $('html, body').animate({scrollTop: targetOffset}, 400);
+           return false;
+         });
+      }
+    }
+  });
+});
+
+$("#nav-mobile li").click(function() {
+  $('.button-collapse').sideNav('hide'); });
